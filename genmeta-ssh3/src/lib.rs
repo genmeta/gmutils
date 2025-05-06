e1474848acf4e8794fd2a38924742126eb942893
eareimu <reimu@genmeta.net>
2025-05-06 16:32:46 +0800

    feat(ssh): support uppercase and optimize code

--- genmeta-ssh3/src/lib.rs
+++ genmeta-ssh3/src/lib.rs
@@ -1,6 +1,6 @@
 use std::{io::Write, net::SocketAddr, time::Duration};
 
-use bytes::Buf;
+use bytes::{Buf, Bytes};
 use clap::Parser;
 use crossterm::{
     event::{self, Event, EventStream, KeyCode, KeyEvent, KeyModifiers},
@@ -9,7 +9,7 @@ use crossterm::{
 };
 use futures::{SinkExt, StreamExt, channel::mpsc};
 use gateway::{Resolver, dns::UdpResolver, localhost::TraversalFactory};
-use gm_quic::ToCertificate;
+use gm_quic::{QuicClient, ToCertificate};
 use http::Uri;
 use serde::{Deserialize, Serialize};
 use tokio::time;
@@ -18,7 +18,6 @@ use tracing::{error, info};
 // 定义客户端与服务器通信的消息结构
 #[derive(Serialize, Deserialize, Debug)]
 enum TerminalMessage {
-    Text(String),
     WindowSize { rows: u16, cols: u16 },
     ControlSequence(String),
     Heartbeat,
@@ -65,85 +64,81 @@ pub async fn run(mut options: Options) -> Result<(), Error> {
     let (username, password) = parse_username_password(&options)?;
 
     // 创建通道用于异步通信
-    let (tx, mut rx) = mpsc::channel::<TerminalMessage>(32);
+    let (mut tx, rx) = mpsc::channel::<TerminalMessage>(32);
 
-    // 启动事件监听任务
-    let event_task = tokio::spawn(handle_event(tx.clone()));
+    // 启动事件监听任务。不需要要await因为它结束时程序也结束了
+    let _event_task = tokio::spawn(handle_event(tx.clone()));
 
     let resolver = UdpResolver::new("1.12.74.4:5300".parse().unwrap());
     let server_name = options.uri.host().ok_or("Missing host in uri")?;
-    let addrs = resolver.look_up(server_name).await?;
+    let server_addrs = resolver.look_up(server_name).await?;
 
-    let mut roots = rustls::RootCertStore::empty();
-    roots.add_parsable_certificates(include_bytes!("../../root.crt").to_certificate());
+    info!("resolved {} to address: {:?}", server_name, server_addrs);
 
-    info!("resolved {} to address: {:?}", server_name, addrs);
+    let quic_client = {
+        let mut roots = rustls::RootCertStore::empty();
+        roots.add_parsable_certificates(include_bytes!("../../root.crt").to_certificate());
 
-    // NAT Traversal
-    let agents = [
-        "1.12.74.4:20004".parse().unwrap(),
-        "[2402:4e00:c011:1700:8624:7e0:5c9a:2]:20004"
-            .parse()
-            .unwrap(),
-    ];
+        let factory = TraversalFactory::with(&[
+            "1.12.74.4:20004".parse().unwrap(),
+            "[2402:4e00:c011:1700:8624:7e0:5c9a:2]:20004"
+                .parse()
+                .unwrap(),
+        ]);
+        let binds = factory
+            .devices()
+            .keys()
+            .filter_map(|device_ip| {
+                device_ip
+                    .parse()
+                    // TODO 此处使用 0 端口, 测试通过, 但不太确定是否有什么问题
+                    .map(|ip| SocketAddr::new(ip, 0))
+                    .inspect_err(|e| error!("Invalid device IP {}: {:?}", device_ip, e))
+                    .ok()
+            })
+            .collect::<Vec<_>>();
+        QuicClient::builder()
+            .with_root_certificates(roots)
+            .without_cert()
+            .with_alpns(["h3"])
+            .with_iface_factory(factory)
+            .with_parameters(client_parameters())
+            .enable_sslkeylog()
+            .bind(&binds[..])
+            .inspect_err(|e| {
+                error!("bind addrs: {binds:?}  err {e:?}");
+            })?
+            .build()
+    };
 
-    let factory = TraversalFactory::with(&agents[..]);
-
-    let mut binds = Vec::new();
-
-    for device_ip in factory.devices().keys() {
-        let device_ip = match device_ip.parse() {
-            Ok(ip) => ip,
-            Err(e) => {
-                error!("Invalid device IP {}: {:?}", device_ip, e);
-                continue;
+    let (quic_conn, mut h3_conn, mut h3_client) = {
+        info!(server_name, ?server_addrs, "connect to server");
+        let mut connect_result = Result::Err(Error::from("Dns not found"));
+        for server_addr in server_addrs {
+            let connect = async {
+                let quic_conn = quic_client.connect(server_name, server_addr)?;
+                let (h3_conn, h3_client) =
+                    h3::client::new(h3_shim::QuicConnection::new(quic_conn.clone()).await).await?;
+                Result::<_, Error>::Ok((quic_conn, h3_conn, h3_client))
+            };
+            match time::timeout(Duration::from_secs(3), connect).await {
+                Ok(Ok(connect)) => {
+                    connect_result = Ok(connect);
+                    break;
+                }
+                Ok(Err(error)) => connect_result = Err(error),
+                Err(_timeout) => connect_result = Err("Connect timeout".into()),
             }
-        };
-        // TODO 此处使用 0 端口, 测试通过, 但不太确定是否有什么问题
-        binds.push(SocketAddr::new(device_ip, 0));
-    }
-
-    let quic_client = ::gm_quic::QuicClient::builder()
-        .with_root_certificates(roots)
-        .without_cert()
-        .with_alpns(["h3"])
-        .with_iface_factory(factory)
-        .with_parameters(client_parameters())
-        .enable_sslkeylog()
-        .bind(&binds[..])
-        .inspect_err(|e| {
-            error!("bind addrs: {binds:?}  err {e:?}");
-        })?
-        .build();
-
-    info!(server_name, ?addrs, "connect to server");
-
-    let mut connect_result = Result::Err(Error::from("Dns not found"));
-    for addr in addrs {
-        let connect = async {
-            let quic_conn = quic_client.connect(server_name, addr)?;
-            let (h3_conn, h3_client) =
-                h3::client::new(h3_shim::QuicConnection::new(quic_conn.clone()).await).await?;
-            Result::<_, Error>::Ok((quic_conn, h3_conn, h3_client))
-        };
-        match time::timeout(Duration::from_secs(3), connect).await {
-            Ok(Ok(connect)) => {
-                connect_result = Ok(connect);
-                break;
-            }
-            Ok(Err(error)) => connect_result = Err(error),
-            Err(_timeout) => connect_result = Err("Connect timeout".into()),
         }
-    }
-
-    let (quic_conn, mut h3_conn, mut h3_client) = connect_result?;
-    let conn_close_monitor = h3_conn.wait_idle();
+        connect_result?
+    };
 
     // 构建 Basic Auth 头
-    use base64::Engine;
-
-    let credentials = format!("{username}:{password}");
-    let basic_auth = base64::engine::general_purpose::STANDARD.encode(credentials.as_bytes());
+    let basic_auth = {
+        use base64::Engine;
+        let credentials = format!("{username}:{password}");
+        base64::engine::general_purpose::STANDARD.encode(credentials.as_bytes())
+    };
 
     info!(%options.uri, "request");
     let request = http::Request::builder()
@@ -157,87 +152,89 @@ pub async fn run(mut options: Options) -> Result<(), Error> {
     let mut stream = h3_client.send_request(request).await?;
     let response = stream.recv_response().await?;
     info!(?response, "received");
+    if response.status() != 200 {
+        return Err(format!("Server response status: {}", response.status()).into());
+    }
 
     // 初始化终端
     // execute!(std::io::stdout(), EnterAlternateScreen, EnableMouseCapture)?;
     terminal::enable_raw_mode()?;
 
-    let (mut sender, mut receiver) = stream.split();
+    let (sender, receiver) = stream.split();
 
-    // read from stdin and write to the stream
-    let send_task = tokio::spawn(async move {
-        let mut interval = tokio::time::interval(Duration::from_secs(30));
-        loop {
-            tokio::select! {
-                msg = rx.next() => match msg {
-                    Some(msg) => {
-                        let serialized = serde_json::to_vec(&msg).unwrap();
-                        if let Err(e) = sender.send_data(serialized.into()).await {
-                            eprintln!("Write to peer error: {e}");
-                            break;
-                        }
-                    }
-                    None => {
-                        if let Err(e) = sender.finish().await {
-                            eprintln!("Finish stream error: {e}");
-                        }
-                        break;
-                    }
-                },
-                _ = interval.tick() => {
-                    let serialized = serde_json::to_vec(&TerminalMessage::Heartbeat).unwrap();
-                    if let Err(e) = sender.send_data(serialized.into()).await {
-                        eprintln!("Send heartbeat error: {e}");
-                        break;
-                    }
-                }
-            }
-        }
-    });
-
-    // receive data from the stream and write to stdout
-    let recv_task = tokio::spawn({
-        let mut tx = tx.clone();
-        async move {
-            let stdout = std::io::stdout();
-            loop {
-                match receiver.recv_data().await {
-                    Ok(Some(chunk)) => {
-                        let response = String::from_utf8_lossy(chunk.chunk());
-                        execute!(stdout.lock(), crossterm::style::Print(response)).unwrap();
-                        stdout.lock().flush().unwrap();
-                    }
-                    Ok(None) => {
-                        break;
-                    }
-                    Err(e) => {
-                        eprintln!("Read from peer error: {e}");
-                        receiver.stop_sending(h3::error::Code::H3_NO_ERROR);
-                        break;
-                    }
-                }
-            }
-            // 接收关闭了，连带着发送也关闭
-            tx.close_channel();
-        }
-    });
-
-    // 等待所有任务完成（通常不会主动退出）
     tokio::select! {
-        _ = event_task => (),
+        // read and encode messages from stdin
+        // _ = event_task => (),
+        // write messages to the stream
+        _ = tokio::spawn(send(sender, rx)) => (),
+        // receive data from the stream and write to stdout
+        _ = tokio::spawn(recv(receiver)) => (),
+        // wait for the quic connection to be terminated
         _ = quic_conn.terminated() => (),
-        _ = conn_close_monitor => (),
+        // wait for the h3 connection to be closed
+        _ = h3_conn.wait_idle() => (),
     }
-
-    if let Err(e) = tokio::try_join!(send_task, recv_task) {
-        eprintln!("Error: {e}");
-    }
+    // close the input channel when the connection is closed
+    tx.close_channel();
 
     // 清理
-    // execute!(std::io::stdout(), LeaveAlternateScreen, DisableMouseCapture)?;
     terminal::disable_raw_mode()?;
 
     Ok(())
+}
+
+async fn send(
+    mut sender: h3::client::RequestStream<h3_shim::SendStream<Bytes>, Bytes>,
+    mut rx: mpsc::Receiver<TerminalMessage>,
+) {
+    let mut interval = tokio::time::interval(Duration::from_secs(30));
+    loop {
+        tokio::select! {
+            msg = rx.next() => match msg {
+                Some(msg) => {
+                    let serialized = serde_json::to_vec(&msg).unwrap();
+                    if let Err(e) = sender.send_data(serialized.into()).await {
+                        eprintln!("Send to peer error: {e}");
+                        break;
+                    }
+                }
+                None => {
+                    if let Err(e) = sender.finish().await {
+                        eprintln!("Finish stream error: {e}");
+                    }
+                    break;
+                }
+            },
+            _ = interval.tick() => {
+                let serialized = serde_json::to_vec(&TerminalMessage::Heartbeat).unwrap();
+                if let Err(e) = sender.send_data(serialized.into()).await {
+                    eprintln!("Send heartbeat error: {e}");
+                    break;
+                }
+            }
+        }
+    }
+}
+
+async fn recv(mut receiver: h3::client::RequestStream<h3_shim::RecvStream, Bytes>) {
+    let stdout = std::io::stdout();
+    loop {
+        match receiver.recv_data().await {
+            Ok(Some(chunk)) => {
+                let response = String::from_utf8_lossy(chunk.chunk());
+                execute!(stdout.lock(), crossterm::style::Print(response)).unwrap();
+                stdout.lock().flush().unwrap();
+            }
+            Ok(None) => {
+                break;
+            }
+            Err(e) => {
+                eprintln!("Read from peer error: {e}");
+                receiver.stop_sending(h3::error::Code::H3_NO_ERROR);
+                break;
+            }
+        }
+    }
 }
 
 fn parse_username_password(options: &Options) -> Result<(String, String), Error> {
@@ -267,7 +264,8 @@ async fn handle_event(mut tx: mpsc::Sender<TerminalMessage>) {
     let (cols, rows) = terminal::size().unwrap();
     _ = tx.send(TerminalMessage::WindowSize { rows, cols }).await;
 
-    while let Some(Ok(event)) = EventStream::new().next().await {
+    let mut event_stream = EventStream::new();
+    while let Some(Ok(event)) = event_stream.next().await {
         match event {
             // 处理窗口大小变化
             Event::Resize(cols, rows) => {
@@ -278,10 +276,11 @@ async fn handle_event(mut tx: mpsc::Sender<TerminalMessage>) {
             Event::Key(KeyEvent {
                 code, modifiers, ..
             }) => {
+                tracing::trace!("key event: {code:?} {modifiers:?}");
                 // 处理键盘事件并转换为相应的终端消息
-                let message = match key_event_to_terminal_message(code, modifiers) {
-                    Some(msg) => msg,
-                    None => continue, // 不处理不支持的按键
+                let message = match (code, modifiers).try_into() {
+                    Ok(msg) => msg,
+                    Err(()) => continue, // 不处理不支持的按键
                 };
 
                 // 发送终端消息
@@ -289,6 +288,7 @@ async fn handle_event(mut tx: mpsc::Sender<TerminalMessage>) {
 
                 // 检查连接是否断开
                 if send_result.is_err() {
+                    eprintln!("Connection closed, exiting...");
                     break;
                 }
             }
@@ -299,45 +299,24 @@ async fn handle_event(mut tx: mpsc::Sender<TerminalMessage>) {
     }
 }
 
-/// 将键盘事件转换为终端消息
-fn key_event_to_terminal_message(
-    code: KeyCode,
-    modifiers: KeyModifiers,
-) -> Option<TerminalMessage> {
-    match (code, modifiers) {
-        // 普通字符直接发送为文本
-        (KeyCode::Char(c), KeyModifiers::NONE) => {
-            // 清空输入缓冲区，避免输入延迟
-            while let Ok(true) = event::poll(Duration::from_millis(0)) {
-                let _ = event::read();
-            }
-            Some(TerminalMessage::Text(c.to_string()))
+impl TryFrom<(KeyCode, KeyModifiers)> for TerminalMessage {
+    type Error = ();
+
+    fn try_from((code, modifiers): (KeyCode, KeyModifiers)) -> Result<Self, Self::Error> {
+        // 清空输入缓冲区，避免输入延迟
+        while let Ok(true) = event::poll(Duration::from_millis(0)) {
+            let _ = event::read();
         }
 
-        // 回车和Tab特殊处理
-        (KeyCode::Enter, _) => {
-            // 清空输入缓冲区，避免输入延迟
-            while let Ok(true) = event::poll(Duration::from_millis(0)) {
-                let _ = event::read();
-            }
-            Some(TerminalMessage::Text("\n".to_string()))
-        }
-        (KeyCode::Tab, _) => Some(TerminalMessage::Text("\t".to_string())),
-
-        // Ctrl+C 特殊处理 - 发送 SIGINT 字符
-        (KeyCode::Char('c'), m) if m == KeyModifiers::CONTROL => {
-            Some(TerminalMessage::ControlSequence("\x03".to_string()))
-        }
-
-        // 其他所有键使用控制序列函数处理
-        (code, modifiers) => {
-            key_event_to_control_sequence(code, modifiers).map(TerminalMessage::ControlSequence)
-        }
+        // 对所有键盘事件统一处理：转换为适当的序列
+        Ok(TerminalMessage::ControlSequence(
+            key_event_to_sequence(code, modifiers).ok_or(())?,
+        ))
     }
 }
 
-/// 将键盘事件转换为控制序列
-fn key_event_to_control_sequence(code: KeyCode, modifiers: KeyModifiers) -> Option<String> {
+/// 将键盘事件转换为控制序列或文本
+fn key_event_to_sequence(code: KeyCode, modifiers: KeyModifiers) -> Option<String> {
     // 计算修饰键的数值
     fn calculate_modifier_value(modifiers: KeyModifiers) -> u8 {
         let mut value = 0;
@@ -397,26 +376,26 @@ fn key_event_to_control_sequence(code: KeyCode, modifiers: KeyModifiers) -> Opti
     }
 
     match code {
-        // 字符键的处理
+        // 字符键的统一处理 - 包括普通字符和带修饰键的字符
         KeyCode::Char(c) => {
-            // 无修饰键的普通字符由上层直接处理为文本
-            if modifiers == KeyModifiers::NONE {
-                return None;
+            // 处理无修饰键的普通字符
+            if modifiers == KeyModifiers::NONE || modifiers == KeyModifiers::SHIFT {
+                // 直接返回字符本身
+                return Some(c.to_string());
             }
 
-            // Ctrl+字母键：特殊处理，生成ASCII控制字符（保持兼容性）
+            // Ctrl+字母键：生成ASCII控制字符
             if modifiers == KeyModifiers::CONTROL && c.is_ascii_alphabetic() {
                 let ctrl_code = (c.to_ascii_lowercase() as u8 & 0x1f) as char;
                 return Some(ctrl_code.to_string());
             }
 
-            // Alt+字符键：特殊处理，前缀为ESC（保持兼容性）
+            // Alt+字符键：ESC前缀
             if modifiers == KeyModifiers::ALT {
                 return Some(format!("\x1b{c}"));
             }
 
             // 其他修饰键组合：使用标准 CSI u 格式
-            // 这是现代终端支持的更通用的格式，能处理任意字符和修饰键组合
             let char_code = c as u32;
             let mod_value = calculate_modifier_value(modifiers) + 1;
             Some(format!("\x1b[{char_code};{mod_value}u"))
@@ -426,7 +405,10 @@ fn key_event_to_control_sequence(code: KeyCode, modifiers: KeyModifiers) -> Opti
         KeyCode::Backspace => Some("\x7f".to_string()),
         KeyCode::Delete => Some(with_modifiers("\x1b[3~", modifiers)),
         KeyCode::Esc => Some("\x1b".to_string()),
-        // Enter和Tab由上层代码特殊处理
+
+        // Enter和Tab现在也通过这里处理，而不是特殊处理
+        KeyCode::Enter => Some("\r".to_string()),
+        KeyCode::Tab => Some("\t".to_string()),
 
         // 方向键
         KeyCode::Up => Some(with_modifiers("\x1b[A", modifiers)),
--- genmeta/src/main.rs
+++ genmeta/src/main.rs
@@ -10,7 +10,11 @@ enum Options {
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     tracing_subscriber::fmt()
-        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::builder()
+                .with_default_directive(tracing_subscriber::filter::LevelFilter::OFF.into())
+                .from_env_lossy(),
+        )
         .with_writer(std::io::stderr)
         .init();
 
