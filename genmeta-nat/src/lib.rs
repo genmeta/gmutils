39d3222414a38cf370671687c4a61d3ede6b791c
metah3m <h3mlxf@gmail.com>
2025-06-26 18:13:05 +0800

    feat: nat detection uses domain name

--- genmeta-nat/src/lib.rs
+++ genmeta-nat/src/lib.rs
@@ -1,29 +1,53 @@
-use std::{net::SocketAddr, sync::Arc};
+use std::net::SocketAddr;
 
 use clap::Parser;
-use qtraversal::{iface::Interface, nat::client::Client};
+use qinterface::factory::ProductQuicInterface;
+use qtraversal::{iface::traversal_factory, nat::client::NatType};
+use trust_dns_resolver::TokioAsyncResolver;
 
 #[derive(Parser, Debug, Clone)]
 #[command(name = "nat-detect", version, about)]
 pub struct Options {
     #[arg(
         short,
-        default_value = "0.0.0.0:5379",
-        help = "Bind address to detect NAT"
+        help = "Bind address to detect NAT type",
+        default_value = "0.0.0.0:5379"
     )]
     pub bind: SocketAddr,
-    #[arg(short, default_value = "1.12.74.4:20004", help = "STUN server address")]
-    pub stun_svr: SocketAddr,
+    #[arg(short, default_value = "nat.genmeta.net", help = "STUN server address")]
+    pub server: String,
 }
 
 type Error = Box<dyn core::error::Error + Send + Sync>;
 
 pub async fn run(options: Options) -> Result<(), Error> {
-    let iface = Arc::new(Interface::new(options.bind).expect("failed to bind socket"));
-    let client = Client::new(iface.clone(), options.stun_svr);
-    let outer_addr = client.outer_addr().await.expect("failed to get outer addr");
-    let nat_type = client.nat_type().await?;
+    let servers = nslook_up(options.server.as_str(), options.bind.ip().is_ipv6() as u8).await?;
+    let factory = traversal_factory(&servers);
+    let iface = factory.bind(options.bind)?;
+
+    let external_addr = iface.endpoint_addr().await?.addr();
+    let nat_type: NatType = iface.nat_type().await?.try_into()?;
+
     println!("NAT type: {nat_type:?}");
-    println!("External Address: {outer_addr}");
+    println!("External Address: {external_addr}");
     Ok(())
+}
+
+async fn nslook_up(domain: &str, family: u8) -> Result<Vec<SocketAddr>, Error> {
+    let resolver = TokioAsyncResolver::tokio_from_system_conf()?;
+    let port = 20004;
+    let addrs = if family == 1 {
+        resolver.ipv6_lookup(domain).await.map(|ips| {
+            ips.iter()
+                .map(|ip| SocketAddr::new(ip.0.into(), port))
+                .collect::<Vec<_>>()
+        })
+    } else {
+        resolver.ipv4_lookup(domain).await.map(|ips| {
+            ips.iter()
+                .map(|ip| SocketAddr::new(ip.0.into(), port))
+                .collect::<Vec<_>>()
+        })
+    }?;
+    Ok(addrs)
 }
