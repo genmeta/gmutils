23ff8bcdd29a427955fae1de968c2c3ea45eee52
eareimu <reimu@genmeta.net>
2026-02-28 17:47:21 +0800

    refactor(genmeta): sort subcommands alphabetically
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-opencode)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- genmeta/src/main.rs
+++ genmeta/src/main.rs
@@ -4,7 +4,6 @@ use genmeta_common::error::Whatever;
 #[derive(Parser, Debug, Clone)]
 #[command(version)]
 enum Options {
-    Ssh3(genmeta_ssh3::Options),
     Curl(genmeta_curl::Options),
     Discover(genmeta_discover::Options),
     Doctor {
@@ -17,10 +16,15 @@ enum Options {
     },
     Nslookup(genmeta_nslookup::Options),
     Proxy(genmeta_proxy::Options),
+    Ssh3(genmeta_ssh3::Options),
 }
 
 #[derive(snafu::Snafu, Debug)]
 enum Error {
+    #[snafu(transparent)]
+    Curl { source: genmeta_curl::Error },
+    #[snafu(transparent)]
+    Discover { source: genmeta_discover::Error },
     #[snafu(transparent)]
     Doctor { source: genmeta_doctor::Error },
     #[snafu(transparent)]
@@ -28,13 +32,9 @@ enum Error {
     #[snafu(transparent)]
     Nslookup { source: genmeta_nslookup::Error },
     #[snafu(transparent)]
-    Ssh3 { source: genmeta_ssh3::Error },
-    #[snafu(transparent)]
-    Curl { source: genmeta_curl::Error },
-    #[snafu(transparent)]
-    Discover { source: genmeta_discover::Error },
-    #[snafu(transparent)]
     Proxy { source: genmeta_proxy::Error },
+    #[snafu(transparent)]
+    Ssh3 { source: genmeta_ssh3::Error },
     #[snafu(transparent)]
     Whatever { source: Whatever },
 }
@@ -52,9 +52,9 @@ async fn run(options: Options) -> Result<(), Error> {
         Options::Curl(options) => genmeta_curl::run(options).await?,
         Options::Discover(options) => genmeta_discover::run(options).await?,
         Options::Doctor { options } => genmeta_doctor::run(options).await?,
-        Options::Proxy(options) => genmeta_proxy::run(options).await?,
         Options::Identity { options } => genmeta_identity::run(options).await?,
         Options::Nslookup(options) => genmeta_nslookup::run(options).await?,
+        Options::Proxy(options) => genmeta_proxy::run(options).await?,
         Options::Ssh3(options) => genmeta_ssh3::run(options).await?,
     };
     Ok(())
