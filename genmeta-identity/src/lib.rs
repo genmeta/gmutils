5d8080466ed8c14ef25bcb71d85b0b2d0d660dfe
eareimu <reimu@genmeta.net>
2026-05-20 23:54:02 +0800

    fix: use license cert server in debug builds

--- genmeta-identity/src/lib.rs
+++ genmeta-identity/src/lib.rs
@@ -9,7 +9,4 @@ pub mod cert_server;
 
 pub const REGISTERABLE_SUFFIXES: &[&str] = &["pilot", "lab"];
 
-#[cfg(debug_assertions)]
-pub const DEFAULT_CERT_SERVER_BASE_URL: &str = "http://127.0.0.1:3001";
-#[cfg(not(debug_assertions))]
 pub const DEFAULT_CERT_SERVER_BASE_URL: &str = "https://license.genmeta.net";
